69ccc1428e529484b473522c9990338c6c7536bb
Gautam <gautam.sisodia@gmail.com>
2015-09-17 13:10:05 -0400

    First commit

---
+++ README.rst
@@ -0,0 +1,12 @@
+===============================
+bxpck
+===============================
+
+.. image:: https://img.shields.io/travis/gautsi/bxpck.svg
+        :target: https://travis-ci.org/gautsi/bxpck
+
+bxpck
+
+* Free software: BSD license
+* Documentation: https://bxpck.readthedocs.org.
+
---
+++ tests/test_bxpck.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_bxpck
+----------------------------------
+
+Tests for `bxpck` module.
+"""
+
+import unittest
+
+from bxpck import bxpck
+
+
+class TestBxpck(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
